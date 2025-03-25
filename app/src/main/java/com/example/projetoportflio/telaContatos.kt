cf0ce59b2b9b193f86ce75eeee4c229c8aa107bc
blougouras <luizblougouras5@gmail.com>
2025-03-24 23:45:04 -0300

    aplicação de botões, adição de novas páginas (projetos e contatos).

--- app/src/main/java/com/example/projetoportflio/telaContatos.kt
+++ app/src/main/java/com/example/projetoportflio/telaContatos.kt
@@ -1,39 +1,16 @@
 package com.example.projetoportflio
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var button1: Button
-    lateinit var button2: Button
-
+class telaContatos : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        button1 = findViewById(R.id.button)
-        button1.setOnClickListener {
-            val intent = Intent(
-                this,
-                telaProjetos::class.java
-            )
-        }
-
-        button2 = findViewById(R.id.button2)
-        button2.setOnClickListener {
-            val intent = Intent(
-                this,
-                telaContatos::class.java
-            )
-        }
-
+        setContentView(R.layout.activity_tela_contatos)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
