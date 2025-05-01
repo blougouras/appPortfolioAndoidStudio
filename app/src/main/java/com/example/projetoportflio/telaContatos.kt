c59ff7b43e2a0416cc4b76f4f1a776a1d4f47496
blougouras <luizblougouras5@gmail.com>
2025-05-01 14:09:47 -0300

    projeto finalizado ( adição de links nas páginas contatos e portfólio )

--- app/src/main/java/com/example/projetoportflio/telaContatos.kt
+++ app/src/main/java/com/example/projetoportflio/telaContatos.kt
@@ -1,12 +1,17 @@
 package com.example.projetoportflio
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.HtmlCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.net.toUri
 
 class telaContatos : AppCompatActivity() {
 
@@ -16,6 +21,22 @@ class telaContatos : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_contatos)
+
+        val textView5 = findViewById<TextView>(R.id.textView5)
+        textView5.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = "https://www.linkedin.com/in/luizblougouras".toUri()
+            startActivity(intent)
+        }
+
+        val textView6 = findViewById<TextView>(R.id.textView6)
+        textView6.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = "https://github.com/blougouras".toUri()
+            startActivity(intent)
+        }
+
+
 
         button4 = findViewById(R.id.button4)
         button4.setOnClickListener {
--- app/src/main/java/com/example/projetoportflio/telaProjetos.kt
+++ app/src/main/java/com/example/projetoportflio/telaProjetos.kt
@@ -3,8 +3,10 @@ package com.example.projetoportflio
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,6 +18,34 @@ class telaProjetos : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_tela_projetos)
+
+        val textView7 = findViewById<TextView>(R.id.textView7)
+        textView7.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = "https://github.com/blougouras/navegandoTelas_androidStudio".toUri()
+            startActivity(intent)
+        }
+
+        val textView8 = findViewById<TextView>(R.id.textView8)
+        textView8.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = "https://github.com/blougouras/appPortfolioAndoidStudio".toUri()
+            startActivity(intent)
+        }
+
+        val textView9 = findViewById<TextView>(R.id.textView9)
+        textView9.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = "https://github.com/blougouras/appAgenda".toUri()
+            startActivity(intent)
+        }
+
+        val textView10 = findViewById<TextView>(R.id.textView10)
+        textView10.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = "https://github.com/blougouras/appAPI".toUri()
+            startActivity(intent)
+        }
 
         button3 = findViewById(R.id.button3)
         button3.setOnClickListener {
